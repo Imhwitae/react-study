03288907853297e29ece0a52e182526482a3faa9
hwitae <ghkdgnlxo2@gmail.com>
2024-06-02 14:50:57 +0900

    todo - 2초가 지났을 경우 alert 안보이게 하기

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,5 +1,6 @@
 import {useParams} from "react-router-dom";
 import styled from "styled-components";
+import {useEffect, useState} from "react";
 
 // let YellowButton = styled.button`
     //background: yellow;
@@ -10,15 +11,25 @@ import styled from "styled-components";
 export default function Detail(props) {
     let {id} = useParams();
     let result = props.shoes.find(data => data.id == id);
-    console.log(result);
+    let [count, setCount] = useState(0);
+    useEffect(() => {
+        // 타이머
+        // setTimeout(() = {
+        //
+        // }, 2000);
+    }, []);
 
     return (
         <div className="container">
+            {/* todo - 2초가 지났을 경우 alert 안보이게 하기 */}
+            <div className="alert alert-warning">
+                2초 이내 구매시 할인
+            </div>
             {/*<YellowButton />*/}
             <div className="row">
                 <div className="col-md-6">
                     <img src={result.img} width="100%"/>
-                </div>
+                </div>s
                 <div className="col-md-6">
                     <h4 className="pt-5">{result.title}</h4>
                     <p>{result.content}</p>
